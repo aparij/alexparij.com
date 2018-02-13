331667fac14df79464330888cc29bee3d4bab5f6
Alex Parij <alex@gazelle.company>
2018-02-13 17:39:53 -0500

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+   
+		Github : <a href="http://www.github.com/aparij">aparij</a>
+    <br/>
+    Twitter : <a href="http://www.twitter.com/alex_parij">@alex_parij</a>
+    <br/>
+    
+    Projects:
+    
+    Stackoverflow Jobs tech trends:  <a href="http://www.reallyhyped.com">ReallyHyped.com</a>
+  </div>
+)
+
+export default IndexPage
